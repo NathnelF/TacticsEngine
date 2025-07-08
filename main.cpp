460d5c8f4dee7fab9fb000547bdcf71350b993c3
NathnelF <natefuller8@gmail.com>
2025-07-08 19:39:58 -0400

    added GameEntity Class, std::vector of AllEntities, moving is currently broken (one move works then no subsequent does)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <raylib.h>
 #include <raymath.h>
 #include <iostream>
+#include <vector>
+#include <memory>
 
- struct entity {
+class GameEntity{
+public:
+	
+	// Fields
 	Vector3 position;
 	Vector3 targetPosition;
+	float t;
 	Color color;
 	float size;
 	Mesh mesh;
@@ -12,6 +18,61 @@
 	BoundingBox collider;
 	bool selected;
 	bool isMoving;
+
+	// constructor 
+	GameEntity(Vector3 initialPosition, Color initialColor){
+		size = 2.0f;
+		t = 0.0f;
+		mesh = GenMeshCube(size, size, size);
+		model = LoadModelFromMesh(mesh);
+		collider = GetMeshBoundingBox(mesh);
+		selected = false;
+		isMoving = false;
+		position = initialPosition;
+		color = initialColor;
+		collider.min = (Vector3){ position.x - size / 2.0f,
+		     position.y - size / 2.0f,
+		     position.z - size / 2.0f };
+		collider.max = (Vector3){ position.x + size / 2.0f,
+		     position.y + size / 2.0f,
+		     position.z + size / 2.0f };
+
+
+	}
+	// add destructor?
+	~GameEntity() {
+		UnloadModel(model);
+	}
+
+	void InitiateMove(Vector3 newTargetPosition){
+		if (!isMoving){
+			targetPosition = newTargetPosition;
+			t = 0.0f;
+			isMoving = true;
+		}
+	}
+
+	//methods
+	void UpdateMove(float moveSpeed){
+		if (isMoving){
+			t += GetFrameTime() * moveSpeed;
+			t = fmin(t, 1.0f);
+			position = Vector3Lerp(position, targetPosition, t);
+			collider.min = (Vector3){position.x - 1.0f, position.y - 1.0f, position.z - 1.0f};
+			collider.max = (Vector3){position.x + 1.0f, position.y + 1.0f, position.z + 1.0f};
+
+			if (t >= 1.0f){
+				position = targetPosition;
+				isMoving = false;
+
+			}
+		}
+	}
+
+	void Draw(){
+		DrawModel(model, position, 1.0f, color);
+		DrawBoundingBox(collider, GREEN);
+	}
 };
 
 int main() {
@@ -28,30 +89,10 @@ int main() {
 	float cameraDistance = 25.0f;
 	Vector3 cameraTarget = { 0.0f, 0.0f, 0.0f };
 
-	// bool cubeSelected = false;
-	// float cubeSize = 2.0f;
-	// Mesh cubeMesh = GenMeshCube(cubeSize, cubeSize, cubeSize);
-	// Model cubeModel = LoadModelFromMesh(cubeMesh);
-	// Vector3 Player1.position = { 0.0f, 1.0f, 0.0f, }; 
-	// Vector3 targetPosition;
-	// Color cubeColor = MAGENTA;
-	// BoundingBox cubeCollider = GetMeshBoundingBox(cubeMesh);
-	// cubeCollider.min = Vector3Add(cubeCollider.min, Player1.position);
-	// cubeCollider.max = Vector3Add(cubeCollider.max, Player1.position);
-	
-	
-
-
-
-	
-
-
-
 
 	Vector3 planePosition = {0.0f, 0.0f, 0.0f};
 	Vector2 planeSize = {50.0f, 50.0f};
 	Color planeColor = GREEN;
-
 	Mesh planeMesh = GenMeshPlane(planeSize.x, planeSize.y, 0, 0);
 	Model planeModel = LoadModelFromMesh(planeMesh);
 	BoundingBox planeCollider = GetMeshBoundingBox(planeMesh);
@@ -71,67 +112,25 @@ int main() {
 	float snapRotation = 4.0f;
 	SetTargetFPS(60);
 
-	entity Player1;
-	Player1.position = (Vector3){ 0.0f, 1.0f, 0.0f };
-	Player1.size = 2.0f;
-	Player1.selected = false;
-	Player1.color = MAGENTA;
-	Player1.mesh = GenMeshCube(Player1.size, Player1.size, Player1.size);
-	Player1.model = LoadModelFromMesh(Player1.mesh);
-	Player1.collider = GetMeshBoundingBox(Player1.mesh);
-	Player1.collider.min = (Vector3){ Player1.position.x - Player1.size / 2.0f,
-                                      Player1.position.y - Player1.size / 2.0f,
-                                      Player1.position.z - Player1.size / 2.0f };
-	Player1.collider.max = (Vector3){ Player1.position.x + Player1.size / 2.0f,
-                                      Player1.position.y + Player1.size / 2.0f,
-                                      Player1.position.z + Player1.size / 2.0f };
-	Player1.isMoving = false;
-
-	
-	entity Player2;
-	Player2.position = (Vector3){ 20.0f, 1.0f, 10.0f };
-	Player2.size = 2.0f;
-	Player2.selected = false;
-	Player2.color = BLUE;
-	Player2.mesh = GenMeshCube(Player2.size, Player2.size, Player2.size);
-	Player2.model = LoadModelFromMesh(Player2.mesh);
-	Player2.collider = GetMeshBoundingBox(Player2.mesh);
-	Player2.collider.min = (Vector3){ Player2.position.x - Player2.size / 2.0f,
-                                      Player2.position.y - Player2.size / 2.0f,
-                                      Player2.position.z - Player2.size / 2.0f };
-	Player2.collider.max = (Vector3){ Player2.position.x + Player2.size / 2.0f,
-                                      Player2.position.y + Player2.size / 2.0f,
-                                      Player2.position.z + Player2.size / 2.0f };
-	Player2.isMoving = false;
-
-	entity* selectables[2] = {&Player1, &Player2};
-	int numSelectables = sizeof(selectables) / sizeof(selectables[0]);
-	std::cout << "LOOOK HEREEREER " << numSelectables << "\n";
-	entity* currentlySelected = nullptr;
-
-
+	std::vector<std::unique_ptr<GameEntity>> AllEntities;
+	AllEntities.push_back(std::make_unique<GameEntity>(Vector3{0.0f, 1.0f, 0.0f}, MAGENTA));
+	AllEntities.push_back(std::make_unique<GameEntity>(Vector3{5.0f, 1.0f, 0.0f}, BLUE));
+	GameEntity* currentlySelected = nullptr;
 
 	while (!WindowShouldClose()){
 		// std::cout << camera.position.x << "\n";
 		if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)){
 			Ray mouseRay = GetMouseRay(GetMousePosition(), camera);
 			RayCollision collision;
-			// for (int i = 0; i < numSelectables; i++){
-			// 	// selectables[i]->selected = false;
-			// 	selectables[i]->color = MAGENTA;
-			//
-			// 
 			if (currentlySelected != nullptr){
 				currentlySelected->color = MAGENTA;
 			}
-			for (int i = 0; i < numSelectables; i++){
+			for (const auto& entityPtr : AllEntities){
 				//go through each selectable to check if there is a collision.
-				collision = GetRayCollisionBox(mouseRay, selectables[i]->collider);
+				collision = GetRayCollisionBox(mouseRay, entityPtr->collider);
 				if (collision.hit){
-					// selectables[i]->selected = true;
-					// selectables[i]->color = BLACK;
-					currentlySelected = selectables[i];
-					selectables[i]->color = BLACK;
+					currentlySelected = entityPtr.get();
+					entityPtr->color = BLACK;
 					break;
 				}
 				else {
@@ -147,23 +146,14 @@ int main() {
 			collision = GetRayCollisionQuad(mouseRay, planePtOne, planePtTwo, planePtThree, planePtFour);
 			if (collision.hit){
 				//get plane coordinates and then move Player1.position to those coordinates
-				currentlySelected->targetPosition = {collision.point.x, currentlySelected->position.y, collision.point.z};
-				t = 0.0f;
-				currentlySelected->isMoving = true;
+				Vector3 newTargetPosition = {collision.point.x, currentlySelected->position.y, collision.point.z};
+				currentlySelected->InitiateMove(newTargetPosition);
 				}
 			}
-		if (currentlySelected->isMoving){
-			t += GetFrameTime() * moveSpeed;
-			t = fmin(t, 1.0f);
-			currentlySelected->position = Vector3Lerp(currentlySelected->position, currentlySelected->targetPosition, t);
-			currentlySelected->collider.min = (Vector3){currentlySelected->position.x - 1.0f, currentlySelected->position.y - 1.0f, currentlySelected->position.z - 1.0f};
-			currentlySelected->collider.max = (Vector3){currentlySelected->position.x + 1.0f, currentlySelected->position.y + 1.0f, currentlySelected->position.z + 1.0f};
-
-			if (t >= 1.0f){
-				currentlySelected->isMoving = false;
-				currentlySelected->position = currentlySelected->targetPosition;
-				}
-			}
+		}
+		//Update position loop for each entity
+		for (const auto& entityPtr: AllEntities){
+			entityPtr->UpdateMove(moveSpeed);
 		}
 		//ADD TAB KEY TO CYCLE THROUGH PLAYER CHARACTERS
 		if (IsKeyDown(KEY_W)){
@@ -212,16 +202,13 @@ int main() {
 				DrawPlane(planePosition, planeSize, planeColor);
 				DrawBoundingBox(planeCollider, PINK);
 				DrawGrid(50, 5.0f);
-				DrawModel(Player1.model, Player1.position, 1.0f, Player1.color);
-				DrawBoundingBox(Player1.collider, GREEN);
-				DrawModel(Player2.model, Player2.position, 1.0f, Player2.color);
-				DrawBoundingBox(Player2.collider, GREEN);
+				for (const auto& entityPtr : AllEntities) {
+				    entityPtr->Draw(); // Each entity draws itself
+				}
 		
 			EndMode3D();
 		EndDrawing();
 	}
-	UnloadModel(Player1.model);
-	UnloadModel(Player2.model);
 	UnloadModel(planeModel);
 	CloseWindow();
 	return 0;
